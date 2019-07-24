aca67eacac58686687661abb8c915b716c25f456
ahydorn <adamhydorn@gmail.com>
2019-07-24 13:59:47 -0700

    init commit

---
+++ client/build/precache-manifest.498863a822d7542b85e069f62232ed47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ab7aa94898482c1efd3",
+    "url": "/static/css/main.cbc41393.chunk.css"
+  },
+  {
+    "revision": "8ab7aa94898482c1efd3",
+    "url": "/static/js/main.a07637fd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "acc63b78c605f5df74fb2a9d10914a62",
+    "url": "/index.html"
+  }
+];
